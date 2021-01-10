108a37295ecc25386037bb2f61ed37777365a9a4
dopamine <65059565+dopaminw@users.noreply.github.com>
2021-01-10 20:50:43 +0800

    Update index.py

--- index.py
+++ index.py
@@ -36,4 +36,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension((f'cogs.{filename[:-3]}'))
 
-client.run(TOKEN)
+client.run(Nzk3NDQ4NzcyMjIwMDkyNDE3.X_moCw.eU1Pl_imjKYjYB7ZmJ0HMlFoi9Q)
